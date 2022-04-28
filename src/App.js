c04607f2ba29139e740b34f9185efc9110d93f5f
suisin-deriv <suisin@regentmarkets.com>
2022-04-28 11:26:21 +0800

    updated version

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ const ExpenseProvider = ({children}) => {
     profile:{
       total_balance: 0,
       token: {
-        authorize:'xoNb6vA1zpQe7CY',
+        authorize:'',
       },
       expense_item: []
     },
@@ -31,155 +31,162 @@ const ExpenseProvider = ({children}) => {
     basket_count:0,
 
     getConnected: ()=>{
-      reference.current = new WebSocket("wss://ws.binaryws.com/websockets/v3?app_id=1089")
-      expenses.isLoading = true
-      if(expenses.connected === false){
-        reference.current.onopen = function(evt){
-          reference.current.send(JSON.stringify(expenses.profile.token));
+      if(expenses.profile.token.authorize.length !== 15){
+        alert("Check Your Token again")
+      }
+      else{
+        reference.current = new WebSocket("wss://ws.binaryws.com/websockets/v3?app_id=1089")
+        expenses.isLoading = true
+        if(expenses.connected === false){
+          reference.current.onopen = function(evt){
+            reference.current.send(JSON.stringify(expenses.profile.token));
 
-          reference.current.onmessage = function(msg){
-            var data = JSON.parse(msg.data)
-            console.log(data)
-            
-            expenses.isLoading = false
-            expenses.connected = true
+            reference.current.onmessage = function(msg){
+              var data = JSON.parse(msg.data)
+              
+              if(data.msg_type === "authorize"){
+                if(data.error === undefined){
+                  reference.current.send(JSON.stringify({
+                    balance:1,
+                    subscribe:1
+                  }))
+                  expenses.isLoading = false
+                  expenses.connected = true
+                }
+                else{
+                  alert(data.error.code)
+                  expenses.isLoading = false
+                  expenses.connected = false
+                }
+              }
+              else if(data.msg_type === "balance"){
+                expenses.profile.total_balance = data.balance.balance
+      
+                expenses.get_statement ? reference.current.send(JSON.stringify({
+                  statement: 1,
+                  description: 1,
+                  limit: 5,
+                  offset: 0
+                })) : expenses.get_statement = true
+              }
+              else if(data.msg_type === "statement"){
+                expenses.all_statement.push(
+                  {
+                    "contract_id": data.statement.transactions[0].contract_id,
+                    "amount" : data.statement.transactions[0].amount,
+                    "trade_item" : data.statement.transactions[0].shortcode
+                  }
+                )
+                
+                let expenses_info = []
+                // console.log(expenses.all_statement);
+                var valueArr = expenses.all_statement.map(function(item){return item.contract_id})
+                const toFindDuplicates = arry => arry.filter((item, index) => arry.indexOf(item) !== index)
+                const duplicateElements = toFindDuplicates(valueArr);
+                // console.log(duplicateElements)
+                let count_synth = 0
+                let count_frx = 0
+                let count_com = 0
+                let count_crypt = 0
+                let count_sind = 0
+                let count_bind = 0
+      
+                duplicateElements.forEach(
+                  (item) => {
+                    console.log(duplicateElements)
+                    let calculate_expense = 0
+                    let trade_item = ""
+                    let color = ""
+                    let gainloss = ""
+      
+                    expenses.all_statement.forEach(
+                      (thing) => {
+                        if(thing.contract_id === item){
+                          calculate_expense = calculate_expense + thing.amount
+                          if(thing.trade_item.includes("FRXX")){
+                            trade_item = "Commodities"
+                          }else if(thing.trade_item.includes("OTC")){
+                            trade_item = "Stock Indices"
+                          }else if(thing.trade_item.includes("WLD")){
+                            trade_item = "Basket Indices"
+                          }else if(thing.trade_item.includes("1HZ")){
+                            trade_item = "Synthetic Indices"
+                          }else if(thing.trade_item.includes("CRY")){
+                            trade_item = "Cryptocurrencies"
+                          }else{
+                            trade_item = "Forex"
+                          }
+                        }
+                      }
+                    )
+      
+                    if(calculate_expense < 0){
+                      color="red"
+                      gainloss = "LOSS"
+                    }else if(calculate_expense > 0){
+                      color="green"
+                      gainloss = "GAIN"
+                    }else{
+                      color="black"
+                      gainloss = "NULL"
+                    }
+      
+                    if(trade_item === "Synthetic Indices"){
+                      count_synth++
+                      expenses.synth_count = count_synth
+                    }else if(trade_item === "Commodities"){
+                      count_com++
+                      expenses.com_count = count_com
+                    }else if(trade_item === "Stock Indices"){
+                      count_sind++
+                      expenses.stock_count = count_sind
+                    }else if(trade_item === "Basket Indices"){
+                      count_bind++
+                      expenses.basket_count = count_bind
+                    }else if(trade_item === "Cryptocurrencies"){
+                      count_crypt++
+                      expenses.crypt_count = count_crypt
+                    }else{
+                      count_frx++
+                      expenses.frx_count = count_frx
+                    }
+      
+                    expenses_info.push({
+                      "expense": calculate_expense.toFixed(2),
+                      "category": trade_item,
+                      "color":color,
+                      "gain_loss": gainloss
+                    })
+      
+                    // console.log(expenses_info)
+      
+                    expenses.profile.expense_item = expenses_info
+                  }
+                )
+              }
+
+            }
+
           }
         }
-      }else{
-        expenses.isLoading = false
-        reference.current.close()
-        reference.current.onclose = function(msg){
-          console.log("Successfully Closed Websocket"+msg)
-        }
-        expenses.connected = false
-        expenses.profile.total_balance = 0
-        expenses.profile.expense_item = []
-        expenses.get_statement = false
-        expenses.all_statement = []
-        expenses.calculate_balance = false
-        expenses.synth_count= 0
-        expenses.frx_count=0
-        expenses.com_count=0
-        expenses.crypt_count=0
-        expenses.stock_count=0
-        expenses.basket_count=0
-      }
-    },
-
-    sendRequest: ()=>{
-      reference.current.send(JSON.stringify({
-        balance:1,
-        subscribe:1
-      }))
-
-      reference.current.onmessage = function(msg){
-        var data = JSON.parse(msg.data)
-
-        if(data.msg_type === "balance"){
-          expenses.profile.total_balance = data.balance.balance
-
-          expenses.get_statement ? reference.current.send(JSON.stringify({
-            statement: 1,
-            description: 1,
-            limit: 5,
-            offset: 0
-          })) : expenses.get_statement = true
-        }
-
-        else if(data.msg_type === "statement"){
-          expenses.all_statement.push(
-            {
-              "contract_id": data.statement.transactions[0].contract_id,
-              "amount" : data.statement.transactions[0].amount,
-              "trade_item" : data.statement.transactions[0].shortcode
-            }
-          )
-          
-          let expenses_info = []
-          // console.log(expenses.all_statement);
-          var valueArr = expenses.all_statement.map(function(item){return item.contract_id})
-          const toFindDuplicates = arry => arry.filter((item, index) => arry.indexOf(item) !== index)
-          const duplicateElements = toFindDuplicates(valueArr);
-          // console.log(duplicateElements)
-          let count_synth = 0
-          let count_frx = 0
-          let count_com = 0
-          let count_crypt = 0
-          let count_sind = 0
-          let count_bind = 0
-
-          duplicateElements.forEach(
-            (item) => {
-              console.log(duplicateElements)
-              let calculate_expense = 0
-              let trade_item = ""
-              let color = ""
-              let gainloss = ""
-
-              expenses.all_statement.forEach(
-                (thing) => {
-                  if(thing.contract_id === item){
-                    calculate_expense = calculate_expense + thing.amount
-                    if(thing.trade_item.includes("FRXX")){
-                      trade_item = "Commodities"
-                    }else if(thing.trade_item.includes("OTC")){
-                      trade_item = "Stock Indices"
-                    }else if(thing.trade_item.includes("WLD")){
-                      trade_item = "Basket Indices"
-                    }else if(thing.trade_item.includes("1HZ")){
-                      trade_item = "Synthetic Indices"
-                    }else if(thing.trade_item.includes("CRY")){
-                      trade_item = "Cryptocurrencies"
-                    }else{
-                      trade_item = "Forex"
-                    }
-                  }
-                }
-              )
-
-              if(calculate_expense < 0){
-                color="red"
-                gainloss = "LOSS"
-              }else if(calculate_expense > 0){
-                color="green"
-                gainloss = "GAIN"
-              }else{
-                color="black"
-                gainloss = "NULL"
-              }
-
-              if(trade_item === "Synthetic Indices"){
-                count_synth++
-                expenses.synth_count = count_synth
-              }else if(trade_item === "Commodities"){
-                count_com++
-                expenses.com_count = count_com
-              }else if(trade_item === "Stock Indices"){
-                count_sind++
-                expenses.stock_count = count_sind
-              }else if(trade_item === "Basket Indices"){
-                count_bind++
-                expenses.basket_count = count_bind
-              }else if(trade_item === "Cryptocurrencies"){
-                count_crypt++
-                expenses.crypt_count = count_crypt
-              }else{
-                count_frx++
-                expenses.frx_count = count_frx
-              }
-
-              expenses_info.push({
-                "expense": calculate_expense.toFixed(2),
-                "category": trade_item,
-                "color":color,
-                "gain_loss": gainloss
-              })
-
-              // console.log(expenses_info)
-
-              expenses.profile.expense_item = expenses_info
-            }
-          )
+        else{
+          expenses.isLoading = false
+          reference.current.close()
+          reference.current.onclose = function(msg){
+            console.log("Successfully Closed Websocket"+msg)
+          }
+          expenses.connected = false
+          expenses.profile.total_balance = 0
+          expenses.profile.expense_item = []
+          expenses.get_statement = false
+          expenses.all_statement = []
+          expenses.calculate_balance = false
+          expenses.synth_count= 0
+          expenses.frx_count=0
+          expenses.com_count=0
+          expenses.crypt_count=0
+          expenses.stock_count=0
+          expenses.basket_count=0
         }
       }
     },
@@ -207,28 +214,43 @@ const ExpenseConnection = () => {
         {
           (store.connected ? 
             (
-              <button onClick={store.getConnected} style={{backgroundColor:"red",color:"white"}}>
-                Disconnect
-              </button>
+              <div className='button--container__buttons'>
+                <input 
+                  className='button--container__buttons--textfield'
+                  placeholder={store.profile.token.authorize}
+                  disabled
+                />
+                <button onClick={store.getConnected} style={{backgroundColor:"red",color:"white"}}>
+                  Disconnect
+                </button>
+              </div>
             ):
             (
-              (store.isLoading ? 
-                <button disabled>
-                  Loading...
-                </button> : 
-                <button onClick={store.getConnected}>
-                  Connect
-                </button>
-              )
+              <div className='button--container__buttons'>
+                <input 
+                  className='button--container__buttons--textfield'
+                  placeholder='Token'
+                  onChange={
+                    (e)=>{
+                      store.profile.token.authorize = e.target.value
+                    }
+                  }
+                />
+                {(store.isLoading ? 
+                  <button disabled>
+                    Loading...
+                  </button> : 
+                  <button onClick={store.getConnected}>
+                    Connect
+                  </button>
+                )}
+              </div>
               // <button onClick={store.getConnected}>
               //   Connect
               // </button>
             )
           )
         }
-        <button onClick={store.sendRequest}>
-          Send
-        </button>
       </div>
     </div>
   ))
@@ -236,12 +258,12 @@ const ExpenseConnection = () => {
 
 const ExpenseLocation = () => {
   const store = React.useContext(ExpenseContext)
-  const [synthIsOpen, setSynthIsOpen] = React.useState(false)
-  const [frxIsOpen, setFrxIsOpen] = React.useState(false)
-  const [comIsOpen, setComIsOpen] = React.useState(false)
-  const [cryptIsOpen, setCryptIsOpen] = React.useState(false)
-  const [sindIsOpen, setSindIsOpen] = React.useState(false)
-  const [bindIsOpen, setBindIsOpen] = React.useState(false)
+  const [synthIsOpen, setSynthIsOpen] = React.useState(true)
+  const [frxIsOpen, setFrxIsOpen] = React.useState(true)
+  const [comIsOpen, setComIsOpen] = React.useState(true)
+  const [cryptIsOpen, setCryptIsOpen] = React.useState(true)
+  const [sindIsOpen, setSindIsOpen] = React.useState(true)
+  const [bindIsOpen, setBindIsOpen] = React.useState(true)
   
   return useObserver( () => (
     <>
