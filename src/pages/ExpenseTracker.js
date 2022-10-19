a1f348fe060ff3a0bf0039df81c3d24cdf92c705
tay suisin <suisin@regentmarkets.com>
2022-10-19 16:46:05 +0800

    Update symbols

--- src/pages/ExpenseTracker.js
+++ src/pages/ExpenseTracker.js
@@ -2,11 +2,13 @@ import React from 'react'
 import ExpenseBalance from '../components/ExpenseBalance/ExpenseBalance';
 import ExpenseConnection from '../components/ExpenseButtonConnection/ExpenseConnection';
 import ExpenseLocation from '../components/ExpenseLocation/ExpenseLocation';
+import Charts from '../components/Charts/Charts'
 
 function ExpenseTracker() {
     return (
       <>
         <ExpenseBalance/>
+        <Charts style={{alignItem:'center'}}/>
         <ExpenseConnection/>
         <ExpenseLocation/>
       </>
